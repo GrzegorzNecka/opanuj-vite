1fbc4bb44ae02dbd811510b9f0c454fb75793efe
GrzegorzNecka <global-strzypior@gmail.com>
2025-08-22 14:34:39 +0200

    conf

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ const App = () => {
     <div>
       <div className="bg-white rounded-lg shadow-sm p-4 mb-4">
         <h1 className="text-xl font-bold text-center">🚀 Rick and Morty - Fan Service</h1>
+        <h2> name: {__APP_NAME__}</h2>
+        <h2> version: {__APP_VERSION__}</h2>
       </div>
       <Outlet />
     </div>
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,30 @@
-import { defineConfig } from 'vite';
+import { defineConfig, splitVendorChunkPlugin } from 'vite';
 import react from '@vitejs/plugin-react';
 import packageJson from './package.json';
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 export default defineConfig({
+  base: '/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: true,
+  },
+
   define: {
     __APP_VERSION__: JSON.stringify(packageJson.version),
+    __APP_NAME__: JSON.stringify(packageJson.name),
   },
   server: {
     port: 3000,
   },
-  plugins: [react()],
+  plugins: [
+    react(),
+    splitVendorChunkPlugin(),
+    ViteImageOptimizer({
+      png: {
+        quality: 80,
+      },
+    }),
+  ],
 });
